e26ed832cb8be4292a7770089e0fe8049e40a448
leo123444 <leo1994448@gmail.com>
2017-02-05 22:11:53 +0000

    Update Mediator.java

--- AppCount-master/app/src/main/java/es/ulpgc/da/appcount/Mediator.java
+++ AppCount-master/app/src/main/java/es/ulpgc/da/appcount/Mediator.java
@@ -1,5 +1,21 @@
 public class Mediator{
 
+ private Integer counter;
 
+    public Mediator() {
+        counter=0;
+    }
+
+    public Integer getCounter() {
+        return counter;
+    }
+
+    public void increment() {
+        this.counter++;
+    }
+
+    public void decrement() {
+        this.counter--;
+    }
 
 }
