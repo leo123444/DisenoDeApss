ce398af0619d9106998d72057c48463330198608
DESKTOP-T85G3V9\eleonora <leo1994448@gmail.com>
2017-02-06 19:03:23 +0000

    Initial commit

---
+++ app/src/main/java/es/ulpgc/eite/android/quiz/Presentador.java
@@ -0,0 +1,142 @@
+package es.ulpgc.eite.android.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Presentador extends AppCompatActivity{
+   Modelo modelo;
+    Vista vista;
+  boolean answerBtnClicked;
+
+boolean confirmBtnClicked;
+
+ public Presentador(){
+
+        this.modelo=new Modelo();
+        this.vista=new Vista();
+
+
+
+ }
+
+    public Modelo getQuestionStore() {
+        return modelo;
+    }
+
+
+    public String getCurrentAnswer() {
+        if(modelo.quizAnswers[modelo.quizIndex] == modelo.userAnswers[modelo.quizIndex]) {
+            return vista.correctLabel;
+        } else {
+            return vista.incorrectLabel;
+        }
+    }
+
+    void onScreenStarted() {
+        //quizApp = (QuizApp) getApplication();
+
+
+       vista. setButtonLabels();
+        checkVisibility();
+
+
+        if(isAnswerBtnClicked()){
+          vista.  setAnswer(getCurrentAnswer());
+        }
+    }
+    void onFalseBtnClicked() {
+        finish();
+        //quizApp.backToQuestionScreen(this);
+    }
+
+    void onTrueBtnClicked() {
+      vista.  setAnswer(getAnswer());
+        vista.setAnswerVisibility(true);
+        confirmBtnClicked = true;
+        checkAnswerVisibility();
+    }
+
+
+
+
+
+    private void checkAnswerVisibility(){
+        if(isAnswerVisible()==false) {
+           vista. hideAnswer();
+        } else {
+          vista.  showAnswer();
+        }
+    }
+
+
+    private void checkToolbarVisibility(){
+        if (!isToolbarVisible()) {
+           vista. hideToolbar();
+        }
+    }
+
+    private void checkVisibility(){
+        checkToolbarVisibility();
+        checkAnswerVisibility();
+    }
+
+    String getConfirmLabel() {
+        return vista.confirmLabel;
+    }
+
+
+
+
+
+    private String getAnswer() {
+        if(modelo.trueAnswer) {
+            return vista.trueLabel;
+        } else {
+            return vista.falseLabel;
+        }
+    }
+
+    String getTrueLabel() {
+        return vista.trueLabel;
+    }
+    public String getCheatLabel() {
+        return vista.cheatLabel;
+    }
+
+
+
+    public String getCurrentQuestion() {
+        return vista.quizQuestions[modelo.quizIndex];
+    }
+
+    public String getFalseLabel() {
+        return vista.falseLabel;
+    }
+
+    public String getNextLabel() {
+        return vista.nextLabel;
+    }
+
+    public String getNextQuestion() {
+        modelo.quizIndex++;
+        return getCurrentQuestion();
+    }
+
+
+    public boolean isAnswerBtnClicked() {
+        return answerBtnClicked;
+    }
+
+    public boolean isAnswerVisible() {
+        return vista.answerVisible;
+    }
+
+    public boolean isToolbarVisible() {
+        return vista.toolbarVisible;
+    }
+    private void setAnswer(boolean answer){
+        modelo.trueAnswer = answer;
+    }
+
+
+
+}
